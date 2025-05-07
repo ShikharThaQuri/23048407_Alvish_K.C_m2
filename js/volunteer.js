6683f90669a1f786d673d1bde4b4586f852acdd7
ShikharThaQuri <shikharchand4321@gmail.com>
2025-05-07 17:51:12 +0545

    update

--- js/volunteer.js
+++ js/volunteer.js
@@ -6,6 +6,24 @@ var submitBtn = document.getElementById("subBtn");
 
 var span = document.getElementsByClassName("volClose")[0];
 
+var fname = document.getElementById("fnamep");
+var lname = document.getElementById("lnamep");
+var email = document.getElementById("emailp");
+var phoneno = document.getElementById("phonenop");
+var saddress = document.getElementById("saddressp");
+var city = document.getElementById("cityp");
+var state = document.getElementById("statep");
+var zip = document.getElementById("zipp");
+
+var one = document.getElementById("fname");
+var two = document.getElementById("lname");
+var three = document.getElementById("email");
+var four = document.getElementById("phoneno");
+var five = document.getElementById("saddress");
+var six = document.getElementById("city");
+var seven = document.getElementById("state");
+var eight = document.getElementById("zip");
+
 btn.onclick = function () {
   modal.style.display = "block";
 };
@@ -17,11 +35,39 @@ span.onclick = function () {
 submitBtn.onsubmit = function (event) {
   event.preventDefault();
 
-  subModal.style.display = "block";
-  setTimeout(timeOut, 1500);
+  checkValue(fname, one);
+  checkValue(lname, two);
+  checkValue(email, three);
+  checkValue(phoneno, four);
+  checkValue(saddress, five);
+  checkValue(city, six);
+  checkValue(state, seven);
+  checkValue(zip, eight);
 
-  submitBtn.reset();
+  if (
+    !one.value == "" &&
+    !two.value == "" &&
+    !three.value == "" &&
+    !four.value == "" &&
+    !five.value == "" &&
+    !six.value == "" &&
+    !seven.value == "" &&
+    !eight.value == ""
+  ) {
+    subModal.style.display = "block";
+    setTimeout(timeOut, 1500);
+
+    submitBtn.reset();
+  }
 };
+
+function checkValue(valueName2, valueName1) {
+  if (valueName1.value == "") {
+    valueName2.style.opacity = "1";
+  } else {
+    valueName2.style.opacity = "0";
+  }
+}
 
 function timeOut() {
   subModal.style.display = "none";
